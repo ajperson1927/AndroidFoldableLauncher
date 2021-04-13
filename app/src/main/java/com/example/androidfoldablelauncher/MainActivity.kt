d5b815ec51071ae5c4aa9090fc48ee163178eb9a
pontiaj2066 <pontiaj2066@gmail.com>
2021-04-13 13:19:26 -0600

    App shows folding status when changed

--- app/src/main/java/com/example/androidfoldablelauncher/MainActivity.kt
+++ app/src/main/java/com/example/androidfoldablelauncher/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.androidfoldablelauncher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.window.WindowManager
+import com.example.androidfoldablelauncher.databinding.ActivityMainBinding
+import java.lang.StringBuilder
+import java.util.concurrent.Executor
+import androidx.core.util.Consumer
+
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import androidx.window.FoldingFeature
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import androidx.window.WindowLayoutInfo
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var windowManager: WindowManager
+    private lateinit var binding: ActivityMainBinding
+
+    private val stateLog: StringBuilder = StringBuilder()
+    private val stateContainer = StateContainer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        windowManager = WindowManager(this)
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        windowManager.registerLayoutChangeCallback(Executor { r: Runnable -> Handler(Looper.getMainLooper()).post(r) }, stateContainer)
+    }
+    internal fun updateStateLog(layoutInfo: WindowLayoutInfo) {
+        stateLog.append(" ").append(layoutInfo).append("\n")
+        binding.stateUpdateLog.text = stateLog
+
+    }
+
+
+
+
+    inner class StateContainer : Consumer<WindowLayoutInfo> {
+        var lastLayoutInfo: WindowLayoutInfo? = null
+
+        override fun accept(newLayoutInfo: WindowLayoutInfo) {
+            updateStateLog(newLayoutInfo)
+            //lastLayoutInfo = newLayoutInfo
+        }
     }
 }
