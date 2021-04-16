640c874b66abd12dc1bf5d989acbcb27cc56ebc5
pontiaj2066 <pontiaj2066@gmail.com>
2021-04-16 17:22:07 -0600

    The selected launcher in both spinners is now saved, and loaded when the app opens

--- app/src/main/java/com/example/androidfoldablelauncher/MainActivity.java
+++ app/src/main/java/com/example/androidfoldablelauncher/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.androidfoldablelauncher;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,12 +16,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Consumer;
 import androidx.window.WindowLayoutInfo;
 import androidx.window.WindowManager;
+import androidx.window.WindowMetrics;
 
 import com.example.androidfoldablelauncher.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
 
@@ -34,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
     private StringBuilder stateLog;
     private StateContainer stateContainer;
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    Spinner foldedSpinner;
+    Spinner unfoldedSpinner;
+
+    TextView textView;
+
+    //Shared preferences strings
+    private final String prefsString = "LauncherSettings";
+    private final String foldedString = "foldedSpinner";
+    private final String unfoldedString = "unfoldedSpinner";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
         packageManager = getPackageManager();
         stateContainer = new StateContainer();
         stateLog = new StringBuilder();
+
+        //preferences
+        sharedPreferences = getSharedPreferences(prefsString,MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        //spinners
+        foldedSpinner = findViewById(R.id.foldedSpinner);
+        unfoldedSpinner = findViewById(R.id.unfoldedSpinner);
+
+        textView = findViewById(R.id.textView);
     }
 
     @Override
@@ -70,34 +91,51 @@ public class MainActivity extends AppCompatActivity {
         //This creates a list of package names for installed launchers. It's intended to be used
         // in a spinner, so the first element is blank for the purpose of the spinner starting out blank
         List<String> launcherList = new ArrayList<>();
-        launcherList.add(" ");
+        launcherList.add("Please select a launcher");
 
         for (ResolveInfo resolveInfo : resolveInfoList) {
             launcherList.add(resolveInfo.activityInfo.packageName);
         }
 
-        //Creates an array adapter for the spinners, the sets the spinners' adapter to it
+        //Creates an array adapter for the spinners, then sets the spinners' adapter to it
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, launcherList);
-        Spinner foldedSpinner = findViewById(R.id.foldedSpinner);
-        Spinner unfoldedSpinner = findViewById(R.id.unfoldedSpinner);
         foldedSpinner.setAdapter(arrayAdapter);
         unfoldedSpinner.setAdapter(arrayAdapter);
 
-
+        //Loads the saved spinner states into the spinners
+        foldedSpinner.setSelection(sharedPreferences.getInt(foldedString, -1));
+        unfoldedSpinner.setSelection(sharedPreferences.getInt(unfoldedString, -1));
 
     }
 
-    //This updates the log on screen with the current display features when called
-    void updateStateLog(WindowLayoutInfo layoutInfo) {
-        stateLog.append(" ").append(layoutInfo).append("\n");
-        binding.stateUpdateLog.setText(stateLog);
+    //Saves the currently selected launcher for each spinner then calls the launcher launcher
+    public void launchAndSave(View view) {
+        editor.putInt(foldedString,foldedSpinner.getSelectedItemPosition());
+        editor.putInt(unfoldedString,unfoldedSpinner.getSelectedItemPosition());
+        editor.apply();
+
+        launchLauncher();
     }
 
+    //Discards changes then calls the launcher launcher
+    public void launchNoSave(View view) {
+        launchLauncher();
+    }
+
+    private void launchLauncher() {
+        if (foldedSpinner.getSelectedItemPosition() < 1 || unfoldedSpinner.getSelectedItemPosition() < 1) {
+            textView.setText("Please Select a valid launcher");
+            return;
+        }
+
+
+    }
 
     class StateContainer implements Consumer<WindowLayoutInfo> {
         @Override
         public void accept(WindowLayoutInfo windowLayoutInfo) {
-            updateStateLog(windowLayoutInfo);
+            stateLog.append(" ").append(windowLayoutInfo).append("\n");
+            binding.stateUpdateLog.setText(stateLog);
         }
     }
 }
